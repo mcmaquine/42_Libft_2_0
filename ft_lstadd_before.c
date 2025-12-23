12e62371c012b1418e5c3979117a38adeef9598b
Matheus Maquine <mmaquine@c2r5p1.42sp.org.br>
2025-12-23 17:19:59 -0300

    added ft_lstadd_before

--- ft_lstadd_before.c
+++ ft_lstadd_before.c
@@ -1,31 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_after.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_before.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/23 15:48:41 by mmaquine          #+#    #+#             */
-/*   Updated: 2025/12/23 17:17:40 by mmaquine         ###   ########.fr       */
+/*   Created: 2025/12/23 16:46:32 by mmaquine          #+#    #+#             */
+/*   Updated: 2025/12/23 17:08:43 by mmaquine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-	Add element after right after element current.
+	Add element new right before current. If current is the root node then
+	ft_lstadd_back is called instead.
 */
-void	ft_lstadd_after(t_list *current, t_list *after)
+void	ft_lstadd_before(t_list *current, t_list *new)
 {
-	if (!current || !after)
+	if (!current || !new)
 		return ;
-	if (!current->next)
+	if (!current->prev)
 	{
-		ft_lstadd_front(&current, after);
+		ft_lstadd_back(&current, new);
 		return ;
 	}
-	after->next = current->next;
-	after->prev = current;
-	current->next->prev = after;
-	current->next = after;
+	new->next = current;
+	new->prev = current->prev;
+	current->prev->next = new;
+	current->prev = new;
 }
