ed5d98c34e9726104770a88ce970e84c4d04c83f
Matheus Maquine <mmaquine@c2r5p1.42sp.org.br>
2025-12-29 13:58:53 -0300

    added new list functions

--- ft_lstfirst.c
+++ ft_lstfirst.c
@@ -6,7 +6,7 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 13:49:13 by mmaquine          #+#    #+#             */
-/*   Updated: 2025/12/29 13:56:51 by mmaquine         ###   ########.fr       */
+/*   Updated: 2025/12/29 14:21:43 by mmaquine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 /*
 	Return the element which should be the first element on a list
 */
-t_list	*ft_lstfirst(t_list	*node)
+t_list	*ft_lstfirst(t_list	*lst)
 {
-	if (!node)
-		return (node);
-	while (node->prev)
-		node = node->prev;
-	return (node);
+	if (!lst)
+		return (lst);
+	while (lst->prev)
+		lst = lst->prev;
+	return (lst);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 14:46:16 by mmaquine          #+#    #+#             */
-/*   Updated: 2025/12/23 16:15:50 by mmaquine         ###   ########.fr       */
+/*   Updated: 2025/12/29 14:12:01 by mmaquine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,10 +68,11 @@ long	ft_putnbr_fd(long n, int fd);
 int		ft_putnbr_u(unsigned int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstadd_after(t_list *current, t_list *new);
+t_list	*ft_lstfirst(t_list *lst);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
